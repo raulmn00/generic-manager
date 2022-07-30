85d6cdee9d5eb225cb11a933031693eec83b30c3
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-07-30 18:26:12 -0300

    cria a home e componentiza o botao para criar projeto

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,18 @@
+import saving from "../../img/savings.svg";
+import LinkButton from "../layout/LinkButton";
+import styles from "./Home.module.css";
+
 export default function Home() {
     return (
         <>
-            <h2>Home Component</h2>
+            <section className={styles.homeContainer}>
+                <h1>
+                    Bem-vindo ao <strong>Generic Manager!</strong>
+                </h1>
+                <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+                <LinkButton linkButton="/newproject" textButton="Criar Projeto" />
+                <img src={saving} alt="imagem saving" />
+            </section>
         </>
     );
 }
