329523f9350b2097435fba2bc0ff9304e119cfd8
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-07-31 21:01:50 -0300

    construindo a base da pagina de projetos

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,9 @@
 import Message from "../layout/Message";
 import { useLocation } from "react-router-dom";
+import styles from "./Projects.module.css";
+import Container from "../layout/Container";
+import LinkButton from "../layout/LinkButton";
+
 export default function Projects() {
     const location = useLocation();
     let message = "";
@@ -8,9 +12,19 @@ export default function Projects() {
     }
 
     return (
-        <div>
-            <h2>Ours Projects</h2>
+        <div className={styles.projectContainer}>
+            <div className={styles.titleContainer}>
+                <h2>Ours Projects</h2>
+                <LinkButton
+                    linkButton="/newproject"
+                    textButton="Criar Projeto"
+                />
+            </div>
             {message && <Message type="sucess" text={message} />}
+
+            <Container customClass="start">
+                <p>Projetos....</p>
+            </Container>
         </div>
     );
 }
