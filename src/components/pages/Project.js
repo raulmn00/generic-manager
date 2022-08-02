2cf300cdfdbd1404acc3e69071a468637dce1f9f
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-08-02 11:00:58 -0300

    adiciona o form de edicao e persiste os dados no banco

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -3,6 +3,8 @@ import Loading from "../layout/Loading";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Container from "../layout/Container";
+import ProjectForm from "../project/ProjectForm";
+import Message from "../layout/Message";
 
 export default function Project() {
     const { id } = useParams();
@@ -10,6 +12,10 @@ export default function Project() {
     const [project, setProject] = useState([]);
 
     const [showProjectForm, setShowProjectForm] = useState(false);
+
+    const [message, setMessage] = useState();
+
+    const [typeMessage, setTypeMessage] = useState();
 
     useEffect(() => {
         setTimeout(() => {
@@ -31,30 +37,71 @@ export default function Project() {
         setShowProjectForm(!showProjectForm);
     }
 
+    function editPost(project) {
+        //budget validation
+        if (project.budget < project.costs) {
+            setMessage(
+                "O orçamento não pode ser menor que o custo do projeto!"
+            );
+            setTypeMessage("error");
+            return false;
+        }
+        fetch(`http://localhost:5000/projects/${project.id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(project),
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setProject(data);
+                setShowProjectForm(false);
+                setMessage("Projeto Atualizado com sucesso!");
+                setTypeMessage("success");
+            })
+            .catch((err) => console.log(err));
+    }
+
     return (
         <>
             {project.name ? (
                 <div className={styles.projectDetails}>
                     <Container customClass="column">
+                        {message && (
+                            <Message type={typeMessage} text={message} />
+                        )}
                         <div className={styles.detailsContainer}>
                             <h1>Projeto: {project.name}</h1>
-                            <button className={styles.btn} onClick={toggleProjectForm}>
+                            <button
+                                className={styles.btn}
+                                onClick={toggleProjectForm}
+                            >
                                 {!showProjectForm ? "Editar Projeto" : "Fechar"}
                             </button>
                             {!showProjectForm ? (
                                 <div className={styles.projectInfo}>
                                     <p>
-                                    <span>Categoria:</span> {project.category.name}
+                                        <span>Categoria:</span>{" "}
+                                        {project.category.name}
                                     </p>
                                     <p>
-                                    <span>Total de Orçamento: R$</span> {project.budget}
+                                        <span>Total de Orçamento: R$</span>{" "}
+                                        {project.budget}
                                     </p>
                                     <p>
-                                    <span>Total Utilizado: R$</span> {project.costs}
+                                        <span>Total Utilizado: R$</span>{" "}
+                                        {project.costs}
                                     </p>
                                 </div>
                             ) : (
-                                <div className={styles.projectInfo}>Form para edição</div>
+                                <div className={styles.projectInfo}>
+                                    <ProjectForm
+                                        handleSubmit={editPost}
+                                        btnText="Concluir Edição"
+                                        projectData={project}
+                                    />
+                                </div>
                             )}
                         </div>
                     </Container>
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -10,7 +10,7 @@ import Loading from "../layout/Loading";
 export default function Projects() {
     const [projects, setProjects] = useState([]);
     const [removeLoading, setRemoveLoading] = useState(false);
-    const [projectMessage, setProjectMessage] = useState('');
+    const [projectMessage, setProjectMessage] = useState("");
 
     const location = useLocation();
     let message = "";
@@ -46,7 +46,7 @@ export default function Projects() {
             .then((resp) => resp.json())
             .then(() => {
                 setProjects(projects.filter((project) => project.id !== id));
-                setProjectMessage('Projeto removido com sucesso');
+                setProjectMessage("Projeto removido com sucesso");
             })
             .catch((err) => console.log(err));
     }
@@ -60,9 +60,9 @@ export default function Projects() {
                     textButton="Criar Projeto"
                 />
             </div>
-            {message && <Message type="sucess" text={message} />}
+            {message && <Message type="success" text={message} />}
 
-            {projectMessage && <Message type="sucess" text={projectMessage} />}
+            {projectMessage && <Message type="success" text={projectMessage} />}
 
             <Container customClass="start">
                 {projects.length > 0 &&
